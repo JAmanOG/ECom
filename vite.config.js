99c32ddbe0f57a98394caf4522e14aa21d7d5948
JAmanOG <jaman0120@gmail.com>
2024-09-26 23:34:05 +0530

    Refactor package-lock.json to update dependencies and add terser for minification

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { terser } from 'rollup-plugin-terser'; // Import Terser plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      // Use the Terser plugin in the build process
+      ...terser({
+        compress: {
+          drop_console: true, // Remove console.* calls
+        },
+      }),
+      apply: 'build', // Apply only during the build process
+    },
+  ],
   optimizeDeps: {
-    include: ['html2pdf.js','@material-tailwind/react'],
-  }
-})
+    include: ['html2pdf.js', '@material-tailwind/react'],
+  },
+});
